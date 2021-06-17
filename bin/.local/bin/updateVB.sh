744260180de82a47af8c2c8cdcb86869bbed11c6
Mariano Z <marianoz@zoho.com>
2021-06-17 15:34:12 -0300

    fix: update vs code update script

--- bin/.local/bin/updateVB.sh
+++ bin/.local/bin/updateVB.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 OUT_DIR=/opt/
-URL='https://vscode-update.azurewebsites.net/latest/linux-x64/insider'
+# URL='https://vscode-update.azurewebsites.net/1.51.0-insider/linux-x64/insider'
+URL='https://code.visualstudio.com/sha/download?build=insider&os=linux-x64'
 DIR="$(dirname "$(readlink -f "$0")")"
 TAR_FILE=$DIR/latest.tar.gz
+echo $TAR_FILE
 
 curl -z $TAR_FILE -o $TAR_FILE -L $URL
 sudo tar -xvzf $TAR_FILE -C $OUT_DIR
