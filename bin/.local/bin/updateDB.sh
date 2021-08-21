e70af6da463286556952fff283937f504921f5ad
Mariano Z <marianoz@zoho.com>
2021-08-21 11:05:25 -0300

    feat: remove vscode url from dbeave script

--- bin/.local/bin/updateDB.sh
+++ bin/.local/bin/updateDB.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 OUT_DIR=/opt/
-# URL='https://vscode-update.azurewebsites.net/1.51.0-insider/linux-x64/insider'
 URL='https://dbeaver.io/files/dbeaver-ce-latest-linux.gtk.x86_64.tar.gz'
 DIR="$(dirname "$(readlink -f "$0")")"
 TAR_FILE=$DIR/dbeaver-ce-latest-linux.gtk.x86_64.tar.gz
