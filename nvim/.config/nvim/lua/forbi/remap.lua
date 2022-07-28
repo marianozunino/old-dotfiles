93d2ca1748b2351d6110dac850cae42f17a698db
Mariano Z <marianoz@zoho.com>
2022-07-28 15:56:25 -0300

    feat: update nvim remaps

--- nvim/.config/nvim/lua/forbi/remap.lua
+++ nvim/.config/nvim/lua/forbi/remap.lua
@@ -1,5 +1,6 @@
 local nnoremap = require("forbi.keymap").nnoremap
-local utils = require('telescope.utils')
+local inoremap = require("forbi.keymap").inoremap
+local utils = require("telescope.utils")
 
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
 nnoremap("Y", "yy")
@@ -11,31 +12,30 @@ nnoremap("<leader>j", ":wincmd j<CR>")
 nnoremap("<leader>k", ":wincmd k<CR>")
 nnoremap("<leader>l", ":wincmd l<CR>")
 
-nnoremap("<leader>ps", function()
-    require('telescope.builtin').grep_string({ search = vim.fn.input("Grep For > ")})
+nnoremap("<c-s>", ":w<CR>")
+inoremap("<c-s>", "<Esc>:w<CR>")
 
+nnoremap("<leader>gf", function()
+	require("telescope.builtin").grep_string({ search = vim.fn.input("Grep For > ") })
 end)
 
 nnoremap("<C-p>", function()
-    local _, ret, _ = utils.get_os_command_output({ 'git', 'rev-parse', '--is-inside-work-tree' }) 
-    if ret == 0 then 
-			require('telescope.builtin').git_files()
-    else 
-			require('telescope.builtin').find_files()
-    end 
+	local _, ret, _ = utils.get_os_command_output({ "git", "rev-parse", "--is-inside-work-tree" })
+	if ret == 0 then
+		require("telescope.builtin").git_files()
+	else
+		require("telescope.builtin").find_files()
+	end
 end)
 
 nnoremap("<leader>pw", function()
-    require('telescope.builtin').grep_string { search = vim.fn.expand("<cword>") }
+	require("telescope.builtin").grep_string({ search = vim.fn.expand("<cword>") })
 end)
 
-nnoremap("<leader>pb", function()
-    require('telescope.builtin').buffers()
+nnoremap(";", function()
+	require("telescope.builtin").buffers()
 end)
 
 nnoremap("<leader>vh", function()
-    require('telescope.builtin').help_tags()
+	require("telescope.builtin").help_tags()
 end)
-
-
-
