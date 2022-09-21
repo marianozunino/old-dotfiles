a1fb1faefd7c173dbf6d60ae5bdcae63f71ba07c
Mariano Z <marianoz@zoho.com>
2022-09-21 13:57:18 -0300

    chore: reduce lightup timeout

--- nvim/.config/nvim/lua/forbi/set.lua
+++ nvim/.config/nvim/lua/forbi/set.lua
@@ -50,7 +50,7 @@ local group = vim.api.nvim_create_augroup("highlight_yank", { clear = true })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
-		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 700 })
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 50 })
 	end,
 	group = group,
 })
