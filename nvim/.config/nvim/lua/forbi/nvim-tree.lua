fe11deb5933f1564d69593f163f2c89bd6d66286
Mariano Z <marianoz@zoho.com>
2022-09-25 12:54:30 -0300

    feat: auto close neo-tree && added harpoon indicator

--- nvim/.config/nvim/lua/forbi/nvim-tree.lua
+++ nvim/.config/nvim/lua/forbi/nvim-tree.lua
@@ -14,4 +14,42 @@ require("nvim-web-devicons").setup({
 	-- will get overriden by `get_icons` option
 	default = true,
 })
-require("neo-tree").setup()
+
+require("neo-tree").setup({
+	filesystem = {
+		hijack_netrw_behavior = "open_default",
+		components = {
+			harpoon_index = function(config, node)
+				local Marked = require("harpoon.mark")
+				local path = node:get_id()
+				local succuss, index = pcall(Marked.get_index_of, path)
+				if succuss and index and index > 0 then
+					return {
+						text = string.format(" %d", index),
+						highlight = config.highlight or "NeoTreeDirectoryIcon",
+					}
+				else
+					return {}
+				end
+			end,
+		},
+		renderers = {
+			file = {
+				{ "icon" },
+				{ "name", use_git_status_colors = true },
+				{ "diagnostics" },
+				{ "git_status", highlight = "NeoTreeDimText" },
+				{ "harpoon_index" }, --> This is what actually adds the component in where you want it
+			},
+		},
+	},
+	event_handlers = {
+		{
+			event = "file_opened",
+			handler = function()
+				--auto close
+				require("neo-tree").close_all()
+			end,
+		},
+	},
+})
