402820c3fc28bea8c880e89b487ef261461ac281
Mariano Z <marianoz@zoho.com>
2022-09-25 12:56:20 -0300

    feat: re organize mappings

--- nvim/.config/nvim/lua/forbi/remap.lua
+++ nvim/.config/nvim/lua/forbi/remap.lua
@@ -3,61 +3,34 @@ local inoremap = require("forbi.keymap").inoremap
 local vnoremap = require("forbi.keymap").vnoremap
 local dev_folders = require("forbi.telescope").dev_folders
 local tmux_switcher = require("forbi.telescope").tmux_switcher
-
-nnoremap("<leader>tt", function()
-	dev_folders()
-end)
-
-nnoremap("<leader>qr", function()
-	R("forbi")
-end)
-
-nnoremap("<leader>tm", function()
-	tmux_switcher()
-end)
+local file_browser = require("forbi.telescope").file_browser
 
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
 nnoremap("Y", "yy")
 nnoremap("?", "?\\v")
 nnoremap("/", "/\\v")
 
-nnoremap("<leader>h", ":wincmd h<CR>")
-nnoremap("<leader>j", ":wincmd j<CR>")
-nnoremap("<leader>k", ":wincmd k<CR>")
-nnoremap("<leader>l", ":wincmd l<CR>")
-
 nnoremap("<c-s>", ":w<CR>")
 inoremap("<c-s>", "<Esc>:w<CR>")
+
+nnoremap("<leader><leader>", "<c-^>")
+
+nnoremap("<leader>qr", function()
+	R("forbi")
+end)
 
 nnoremap("<leader>gf", function()
 	require("telescope.builtin").grep_string({ search = vim.fn.input("Grep For > ") })
 end)
 
-nnoremap("<C-p>", function()
-	local utils = require("telescope.utils")
-	local _, ret, _ = utils.get_os_command_output({ "git", "rev-parse", "--is-inside-work-tree" })
-	if ret == 0 then
-		require("telescope.builtin").git_files({
-			show_untracked = true,
-			git_command = {
-				"git",
-				"ls-files",
-				"--exclude-standard",
-				"--cached",
-				"-x",
-				"!*.tfvars",
-				"-x",
-				"!.env",
-			},
-		})
-	else
-		require("telescope.builtin").find_files()
-	end
+nnoremap("<leader>gw", function()
+	require("telescope.builtin").grep_string({ search = vim.fn.expand("<cWORD>") })
 end)
 
-nnoremap("<leader>gw", function()
-	require("telescope.builtin").grep_string({ search = vim.fn.expand("<cword>") })
-end)
+nnoremap("<leader>gr", ":Telescope lsp_references<cr>")
+nnoremap("<leader>tt", dev_folders)
+nnoremap("<leader>gg", tmux_switcher)
+nnoremap("<C-p>", file_browser)
 
 nnoremap(";", require("telescope.builtin").buffers)
 nnoremap("<leader>vh", require("telescope.builtin").help_tags)
@@ -73,10 +46,6 @@ nnoremap("<leader>gc", function()
 	require("neogit").open({ "commit" })
 end)
 
-nnoremap("<leader>m", ":Telescope marks<cr>")
-nnoremap("<leader>gr", ":Telescope lsp_references<cr>")
-nnoremap("<leader><leader>", "<c-^>")
-
 -- jester
 nnoremap("<leader>jr", ":lua require('jester').run()<CR>")
 nnoremap("<leader>jd", ":lua require('jester').debug()<CR>")
@@ -91,26 +60,6 @@ vim.api.nvim_create_user_command("Q", function()
 	vim.api.nvim_command("bd!|qall!")
 end, { nargs = 0 })
 
-nnoremap("<leader>a", require("harpoon.mark").add_file, { silent = true })
-nnoremap("<C-h>", require("harpoon.ui").toggle_quick_menu, { silent = true })
-nnoremap("<leader>tc", require("harpoon.cmd-ui").toggle_quick_menu, { silent = true })
-
-nnoremap("<leader>y", function()
-	require("harpoon.ui").nav_file(1)
-end, { silent = true })
-
-nnoremap("<leader>u", function()
-	require("harpoon.ui").nav_file(2)
-end, { silent = true })
-
-nnoremap("<leader>i", function()
-	require("harpoon.ui").nav_file(3)
-end, { silent = true })
-
-nnoremap("<leader>o", function()
-	require("harpoon.ui").nav_file(4)
-end, { silent = true })
-
 -- move lines
 vnoremap("J", ":m '>+1<CR>gv=gv")
 vnoremap("K", ":m '<-2<CR>gv=gv")
@@ -124,3 +73,34 @@ nnoremap("<C-g>", "<cmd>silent !tmux-switcher<CR>")
 
 -- togle set list F3
 nnoremap("<F3>", ":set list! list?<CR>")
+
+-- reload current lua file
+nnoremap("<leader>rl", function()
+	local file = vim.api.nvim_buf_get_name(0)
+	if file:match("lua") then
+		vim.cmd("luafile " .. file)
+	end
+	print("Reloaded :D")
+end)
+
+-- Harpoon mappings
+
+nnoremap("<leader>a", require("harpoon.mark").add_file, { silent = true })
+nnoremap("<C-h>", require("harpoon.ui").toggle_quick_menu, { silent = true })
+-- nnoremap("<leader>tc", require("harpoon.cmd-ui").toggle_quick_menu, { silent = true })
+
+nnoremap("<leader>1", function()
+	require("harpoon.ui").nav_file(1)
+end, { silent = true })
+
+nnoremap("<leader>2", function()
+	require("harpoon.ui").nav_file(2)
+end, { silent = true })
+
+nnoremap("<leader>3", function()
+	require("harpoon.ui").nav_file(3)
+end, { silent = true })
+
+nnoremap("<leader>4", function()
+	require("harpoon.ui").nav_file(4)
+end, { silent = true })
